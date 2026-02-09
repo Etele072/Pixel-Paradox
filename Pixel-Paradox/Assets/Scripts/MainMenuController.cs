dc208f1c3fce43e99305ee7cdfa629672f9e17b4
RQBence <bence.balpataki@gmail.com>
2026-02-09 15:12:02 +0100

    audioManager complete

--- Pixel-Paradox/Assets/Scripts/MainMenuController.cs
+++ Pixel-Paradox/Assets/Scripts/MainMenuController.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 
 public class MainMenuController : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public Slider masterSlider;
+
     private void Start()
     {
         MusicManager.Instance.PlayMusic("MainMenu");
+        LoadVolume();
+        
     }
 
     public void OnStartClick()
@@ -23,4 +32,39 @@ public class MainMenuController : MonoBehaviour
 #endif
         Application.Quit();
     }
+
+    public void UpdateMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+
+    public void UpdateMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", volume);
+    }
+
+    public void UpdateSoundVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", volume);
+    }
+
+    public void SaveVolume()
+    {
+        audioMixer.GetFloat("MusicVolume", out float musicVolume);
+        PlayerPrefs.SetFloat("MusicVolume", musicVolume);
+
+        audioMixer.GetFloat("SFXVolume", out float sfxVolume);
+        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
+
+        audioMixer.GetFloat("MasterVolume", out float masterVolume);
+        PlayerPrefs.SetFloat("MasterVolume", masterVolume);
+    }
+
+    public void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+        masterSlider.value = PlayerPrefs.GetFloat("MasterVolume");
+    }
+
 }
